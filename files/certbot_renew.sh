596c01c48ddddec4e841cb9ca8585a4048f89ddf
Александр Окунев <a.okunev@mininform74.ru>
2025-02-12 11:44:36 +0500

    fix certbot renew script

--- files/certbot_renew.sh
+++ files/certbot_renew.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 # Stopping nginx container if running
-docker ps -q --filter "name=nginx" | grep -q . && docker stop nginx || true
+if [ "$(docker ps -q --filter "name=nginx")" ]; then
+    docker stop nginx
+fi
 
 # Performing force renewal of TLS certificates using certbot
-docker run -it --rm --name certbot \
+docker run --rm --name certbot \
   -v "/etc/letsencrypt:/etc/letsencrypt" \
   -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
   -v "/var/log/letsencrypt:/var/log/letsencrypt" \
   -p 80:80 \
   certbot/certbot renew --standalone --non-interactive --quiet --force-renewal
 
-# Starting nginx container if exists
-docker ps -a --filter "name=nginx" | grep -q . && docker start nginx || true
+# Stopping nginx container if running
+if [ "$(docker ps -q --filter "name=nginx")" ]; then
+    docker start nginx
+fi
+
+# Restarting v2fly container if running
+if [ "$(docker ps -q --filter "name=v2fly")" ]; then
+    docker restart v2fly
+fi
+
+date
